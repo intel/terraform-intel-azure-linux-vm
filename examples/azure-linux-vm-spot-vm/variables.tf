dc898e5d0e914d3b4da854cf18e9a460835a2832
Kinder Wischmeier <kinder.wischmeier@intel.com>
2023-02-02 14:12:09 -0500

    removed provider block, added password conditions

--- examples/azure-linux-vm-spot-vm/variables.tf
+++ examples/azure-linux-vm-spot-vm/variables.tf
@@ -1,6 +1,9 @@
 variable "admin_password" {
   description = "The Password which should be used for the local-administrator on this virtual machine"
-  default     = null
   type        = string
   sensitive   = true
+  validation {
+    condition     = length(var.admin_password) >= 8
+    error_message = "The admin_password value must be at least 8 characters in length"
+  }
 }
--- provider.tf
+++ provider.tf
@@ -7,7 +7,3 @@ terraform {
     }
   }
 }
-
-provider "azurerm" {
-  features {}
-}
