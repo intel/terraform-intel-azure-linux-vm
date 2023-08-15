02a55b206032c1d81b94677be7bf4d9698896b7f
daveshrestha-intel <dave.shrestha@intel.com>
2023-08-15 12:29:24 -0700

    Created RHEL exampels

---
+++ examples/azure-rhel-vm-spot-vm/main.tf
@@ -0,0 +1,26 @@
+# Example of how to pass variable for database password:
+# terraform apply -var="db_password=..."
+# Environment variables can also be used https://www.terraform.io/language/values/variables#environment-variables
+# Resource azurerm_linux_virtual_machine requires a preconfigured resource group, virtual network, and subnet in Azure
+
+
+module "azurerm_linux_virtual_machine" {
+  source                              = "intel/azure-linux-vm/intel"
+  azurerm_resource_group_name         = "terraform-testing-rg"
+  azurerm_virtual_network_name        = "vnet1"
+  virtual_network_resource_group_name = "terraform-testing-rg"
+  azurerm_subnet_name                 = "default"
+  admin_password                      = var.admin_password
+  source_image_reference_offer        = "RHEL"
+  source_image_reference_sku          = "8-LVM-gen2"
+  source_image_reference_publisher    = "RedHat"
+  source_image_reference_version      = "latest"
+ 
+  priority                            = "Spot"
+  max_bid_price                       = 0.0874
+  eviction_policy                     = "Deallocate"
+  tags = {
+    "owner"    = "user@company.com"
+    "duration" = "1"
+  }
+}
---
+++ examples/azure-rhel-vm-spot-vm/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.41.0"
+    }
+  }
+}
+
+
