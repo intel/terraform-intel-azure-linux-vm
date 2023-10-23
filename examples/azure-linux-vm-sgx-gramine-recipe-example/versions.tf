e86f7397f93b6146d0562c146a0a425f6b1abf73
shreejam21 <shreejan.mistry@intel.com>
2023-10-23 16:47:53 -0700

    Repo Name Changed

---
+++ examples/azure-linux-vm-sgx-gramine-recipe-example/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.41.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">=2.2.0"
+    }
+  }
+}
